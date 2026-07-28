5d1186a276c4645eb031f11c4d192015ca2349ff
Olivier Rossant <orossant@amazon.fr>
2026-07-28 10:36:04 +0200

    chore(auth): reduce Cognito minimum password length from 12 to 10

--- terraform/modules/auth/main.tf
+++ terraform/modules/auth/main.tf
@@ -30,7 +30,7 @@ resource "aws_cognito_user_pool" "main" {
 
   # Password policy
   password_policy {
-    minimum_length    = 12
+    minimum_length    = 10
     require_lowercase = true
     require_numbers   = true
     require_symbols   = true
